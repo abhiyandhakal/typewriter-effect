04ba9489a801fe5a004e7d63c240b60f823fc324
Abhiyan Dhakal <abheeyaandhacaal@gmail.com>
2022-11-12 09:46:16 +0545

    basic typewriter effect

---
+++ script.js
@@ -0,0 +1,43 @@
+const typingText = document.getElementById('typing-text');
+
+const phrase = 'Hello, I am Abhiyan Dhakal';
+
+let i = 0;
+let isCompleted = false;
+let partPhrase = '';
+let speed = 100;
+let isEnd = true;
+
+function checkEnd() {
+	if (isEnd) {
+		speed = 2000;
+		isEnd = false;
+	} else speed = 100;
+}
+
+function loop() {
+	isEnd = false;
+
+	if (i < phrase.length && !isCompleted) {
+		i++;
+		partPhrase = phrase.slice(0, i + 1);
+
+		if (i === phrase.length) isEnd = true;
+	} else if (i >= 0) {
+		isCompleted = true;
+		i--;
+		partPhrase = phrase.slice(0, i + 1);
+	} else {
+		isCompleted = false;
+		i++;
+		isEnd = true;
+	}
+
+	typingText.innerText = partPhrase;
+
+	checkEnd();
+
+	setTimeout(loop, speed);
+}
+
+loop();
